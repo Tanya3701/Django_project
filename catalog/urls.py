c7b9eae144b9c57c6d75f6ae577abeff843a3c5c
Bogomazova Tanya <ip-bogomazova@mail.ru>
2026-02-24 21:42:49 +0500

    homework

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from catalog.apps import CatalogConfig
 
-from . import views
+from .views import base, contacts, home, product_info
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
-    path("contacts/", views.contacts, name="contacts"),
-]
+    path("home/", home, name="home"),
+    path("contacts/", contacts, name="contacts"),
+    path("product_info/<int:pk>/", product_info, name="product_info"),
+    path("base/", base, name="base"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, "home.html")
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "home.html", context)
 
 
 def contacts(request):
@@ -11,3 +15,13 @@ def contacts(request):
         name = request.POST.get("name")
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, "contacts.html")
+
+
+def product_info(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {"product": product}
+    return render(request, "product_info.html", context)
+
+
+def base(request):
+    return render(request, "base.html")
